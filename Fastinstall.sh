a3eaef01be3936745c722acc4f9ef9526572c1e8
spidersr <75587659+spidersr@users.noreply.github.com>
2021-04-19 04:27:49 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -2,6 +2,13 @@
 cd $HOME/spider
 rm -rf $HOME/.telegram-cli
 install() {
+vn =`lsb_release -rs | cut -f1 -d"."`
+if ["$vn" == "18" ];then
+echo -e "Installing depedencies for ubuntu 18  ... \n"
+cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
+wget "http://black-source.tk/BlackTeAM/Api/compat-libevent2-5_2.0.21-1ubuntu18_amd64-1.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
+rm compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
+fi
 rm -rf $HOME/.telegram-cli
 sudo chmod +x tg
 chmod +x spider
