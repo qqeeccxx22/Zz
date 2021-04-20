fef44833409faedefcdca7b83039529af98c4477
spidersr <75587659+spidersr@users.noreply.github.com>
2021-04-20 06:07:40 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -9,13 +9,6 @@ chmod +x ts
 ./ts
 }
 if [ "$1" = "ins" ]; then
-vn =`lsb_release -rs | cut -f1 -d.`
-if ["$vn" == "18" ];then
-echo -e "Installing depedencies for ubuntu 18  ... \n"
-cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
-wget "http://black-source.tk/BlackTeAM/Api/compat-libevent2-5_2.0.21-1ubuntu18_amd64-1.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
-rm compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
-fi
 install
 fi
 chmod +x install.sh
